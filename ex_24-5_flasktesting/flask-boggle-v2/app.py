7226c33a133404d1643fa0b7bf7841f65072c1b1
Nina del Rosario <nina.del@gmail.com>
2023-08-18 21:50:12 -0700

    setting up initial routes and layouts

--- ex_24-5_flasktesting/flask-boggle-v2/app.py
+++ ex_24-5_flasktesting/flask-boggle-v2/app.py
@@ -1,3 +1,57 @@
+from flask import Flask, request, render_template, session, redirect, jsonify
+from random import randint,  choice, sample
+from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "chickenzarecool21837"
+debug = DebugToolbarExtension(app)
+
 boggle_game = Boggle()
+
+@app.route('/')
+def home_page():
+    """initialize a new game and set up game stats"""
+    board = boggle_game.make_board()
+    session['board'] = board
+    # if there's already a running game count, increment the count
+    if session.get('game-count') is not None:
+        session['game-count'] = session['game-count'] + 1
+    else:
+        session['game-count'] = session.get('game-count', 0)
+    session['high-score'] = session.get('high-score', 0)
+    return render_template("index.html", board=board)
+
+# route to get board
+@app.route('/get-board')
+def get_current_board():
+    """api for front-end to access current game board"""
+    board = session['board']
+    result = {"board": board}
+    return jsonify(result)
+
+# route to check word
+# need check_valid_word method on boggle_game object
+# with board and word as arguments
+@app.route('/check-word', methods=['POST'])
+def check_word():
+    """checks submitted word and returns result"""
+    data = request.get_json(silent=True)
+    word = data.get('word')
+    board = session['board']
+    result = boggle_game.check_valid_word(board=board, word=word)
+    return jsonify(result)
+
+# route to check score
+# receives last game score from front end, checks against session high score
+# if new high score, return new high score boolean, high score value
+# update high score and game count in session
+@app.route('/check-score', methods=['POST'])
+def check_score():
+    """check game score against high score"""
+    data = request.get_json(silent=True)
+    score = int(data.get('score'))
+    high_score = int(session.get('high-score', 0))
+    result = {score > high_score}
+    return jsonify(result)
---
+++ ex_24-5_flasktesting/flask-boggle-v2/static/js/boggle.js
@@ -0,0 +1,18 @@
+class BoggleGame {
+  // CONSTUCTOR
+  // properties: boardContainer, score (0), words (empty set), timerDuration
+  // set boardcontainer
+  // set score: initialize to 0
+  // set word list: initialize to empty
+  // METHODS
+  // method: show word
+  // append valid word to displayed word list
+  // method: show message
+  // steps: if timer expires, display message about high score and refresh to play again
+  // method: handle submit
+  // steps: check if valid, adjust format as needed, check if in word list already, Flask API to check submitted word, if valid word display word and update score
+  // tick timer
+  // method: score game
+  // steps: get current game score, Flask API to check high score, update UI accordingly
+  // update stats
+}
